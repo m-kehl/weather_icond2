6c9092b8f156a933cdd0e17900ad1f3a116c3edf
m-kehl <rudolfsinger36@gmail.com>
2024-02-05 11:50:19 +0100

    rename functions for spaceholder so it can be used for all plots (not only for plant_plots)

--- functions/f_plot_spaceholder.R
+++ functions/f_plot_spaceholder.R
@@ -1,4 +1,4 @@
-f_plot_plants_spaceholder <- function(){
+f_plot_spaceholder <- function(){
   plot(c(0, 1), c(0, 1), ann = F, bty = 'n', type = 'n', xaxt = 'n', yaxt = 'n')
   text(x = 0.5, y = 0.5, paste("Please select your input\n"), 
        cex = 1.6, col = "black")
