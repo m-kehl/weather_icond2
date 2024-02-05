11420ed9bdd9188e99fd5ec070c6fe8b53e1f81e
m-kehl <rudolfsinger36@gmail.com>
2024-02-05 09:17:57 +0100

    added waiter in panel Wildpflanzen

--- functions/f_read_plants.R
+++ functions/f_read_plants.R
@@ -1,11 +1,23 @@
 f_read_plants <- function(plant){
+  waiter_show( # show the waiter
+    html = tagList(
+      spin_fading_circles(),
+      "Download data from opendata.dwd.de .."
+    ), # use a spinner
+    id = c("plant_out")
+  )
   
-  phenology_base <- "ftp://opendata.dwd.de/climate_environment/CDC/observations_germany/phenology/annual_reporters/wild/historical/"
-  
-  all_plants <- indexFTP("", base=phenology_base, dir=tempdir(), overwrite = T)
-  
-  ui_plant <- all_plants[grepl(plant,all_plants)]
-  plant_data <- data.table::fread(paste0(phenology_base,ui_plant))
+  if (plant == ""){
+    plant_data = data.frame()
+  } else{
+    phenology_base <- "ftp://opendata.dwd.de/climate_environment/CDC/observations_germany/phenology/annual_reporters/wild/historical/"
+    
+    all_plants <- indexFTP("", base=phenology_base, dir=tempdir(), overwrite = T)
+    
+    ui_plant <- all_plants[grepl(plant,all_plants)]
+    plant_data <- data.table::fread(paste0(phenology_base,ui_plant))
+    
+  }
   #print(plant_data)
   # help_data <- data.table::fread(paste0(phenology_base,
   #                                       "PH_Beschreibung_Phaenologie_Stationen_Jahresmelder.txt"))
