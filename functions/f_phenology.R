7bf00d53c8acccaf1fe0ea5285857a775ffbae85
m-kehl <rudolfsinger36@gmail.com>
2024-02-05 10:46:38 +0100

    clean up function f_phenology and f_plant_eta (both unused)

--- functions/f_phenology.R
+++
@@ -1,6 +0,0 @@
-# #zB Schneeglöckchen
-# gg <- data.table::fread("https://opendata.dwd.de/climate_environment/CDC/observations_germany/phenology/annual_reporters/wild/historical/PH_Jahresmelder_Wildwachsende_Pflanze_Schneegloeckchen_1925_2022_hist.txt")
-# 
-# nwp_base_rain_gsp <- paste0("ftp://opendata.dwd.de/climate_environment/CDC/observations_germany/phenology/annual_reporters/wild/historical/"
-#                               + )
-# nwp_urls <- indexFTP("", base=nwp_base_rain_gsp, dir=tempdir())
