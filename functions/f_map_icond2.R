1a80877d66bf709af2ce10db8d1d5d0aa9dac3cb
m-kehl <rudolfsinger36@gmail.com>
2024-02-13 14:10:18 +0000

    add date and time of map to title

--- functions/f_map_icond2.R
+++ functions/f_map_icond2.R
@@ -17,12 +17,16 @@ f_map_icond2 <- function(input_time,diff_forecasts,parameter,point_coord,point_f
     }
     df_col <- data.frame(from = c(0.3,1,2,4,8,15,30,60), to = c(1,2,4,8,15,30,60,120), color = c("peachpuff1","pink","plum3","maroon1","red","red4","gold","lawngreen"))
 
-    terra::plot(x = subset(diff_forecasts, c(ii)), main = paste0(title_name," [mm/h]"),
+    terra::plot(x = subset(diff_forecasts, c(ii)), main = paste0(title_name," [mm/h] - ",
+                                format(time(subset(diff_forecasts, c(ii)), format= ""),
+                                       "%d.%m.%Y %H:%M")),
                 col = df_col)
   } else{
     #df_col <- data.frame(from = c(-10,-5,0,5,10,15,20,25), to = c(-5,0,5,10,15,20,25,30), color = c("darkblue","dodgerblue","lightblue","salmon","tomato","red","darkorange","gold"))
     
-    terra::plot(x = subset(diff_forecasts, c(ii)), main = "Temperatur [\u00B0C]",
+    terra::plot(x = subset(diff_forecasts, c(ii)), main = paste0("Temperatur [\u00B0C] ",
+                        format(time(subset(diff_forecasts, c(ii)), format= ""),
+                               "%d.%m.%Y %H:%M - ")),
                  range = c(min(minmax(diff_forecasts)),max(minmax(diff_forecasts))))
 
   }
