8c046fe421a0afcf312bdafbd32704ab4e567ded
m-kehl <rudolfsinger36@gmail.com>
2024-03-12 11:29:17 +0000

    tidy up outdated functions

--- functions/f_state_forecast.R
+++
@@ -1,7 +0,0 @@
-f_state_forecast <- function(diff_forecast,square_coord){
-  # 3. process data ---------------------------------------------------------
-
-  crop_forecast <- crop(diff_forecast,square_coord)
-
-  return(crop_forecast)
-}
