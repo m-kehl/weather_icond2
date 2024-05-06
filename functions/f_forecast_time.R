ca98b4d9a10b25d6267af62234be07867342f67c
m-kehl <rudolfsinger36@gmail.com>
2024-05-06 13:36:49 +0000

    bug correction: system time must not be set to CET by command

--- functions/f_forecast_time.R
+++ functions/f_forecast_time.R
@@ -1,7 +1,6 @@
 f_forecast_time <- function(){
   ## function to extract date/time of latest icon d2 forecast calculation
 
-  #Sys.setenv(TZ="CET")
   current_time <- Sys.time()
   current_UTC <- floor_date(as.POSIXct(current_time,"UTC"),"hour")
   current_UTC_hour <- as.integer(format(current_UTC, "%H"))
@@ -9,8 +8,6 @@ f_forecast_time <- function(){
                             ifelse(current_UTC_hour %% 3 == 1,
                                    current_UTC - 4 * 60 * 60,
                                    current_UTC - 3 * 60 * 60)),"UTC")
-  #forecast_hour <- ifelse(nchar(as.character(forecast_hour)),paste0("0",forecast_hour),as.character(forecast_hour))
-                                  
   return(forecast_time)
 }
 
