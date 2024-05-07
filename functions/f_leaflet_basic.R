c5b1fb82a96427b205770fe788be875ed8ba5499
m-kehl <rudolfsinger36@gmail.com>
2024-05-07 13:29:09 +0000

    set view of first map section to fixed values

--- functions/f_leaflet_basic.R
+++ functions/f_leaflet_basic.R
@@ -4,6 +4,7 @@ f_leaflet_basic <- function(){
   leaflet() %>% 
     addProviderTiles(providers$CartoDB.Positron,layerId = "map_click") %>% 
     setMaxBounds(lng1 = -3.834759, lat1 = 43.170241, lng2=20.219425, lat2=58.052226) %>%
+    setView(zoom = 7,lng = 9,lat = 48.5) %>% #setView to Baden-Württemberg as beginning
     # borders of forecast area
     addPolygons(lng = c(-0.326501,5.470697,10.331397,13.519183,17.628903,20.219425,13.030434,7.577484,-3.834759),
                 lat = c(43.170241,43.535726,43.646881,43.600792,43.362164,57.637294,58.032444,58.052226,57.333792),
