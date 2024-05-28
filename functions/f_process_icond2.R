a664d5dd07db47d75052dc64190eb7b1c88dafa4
m-kehl <rudolfsinger36@gmail.com>
2024-05-28 11:32:56 +0000

    convert pressure data from Pa to hPa

--- functions/f_process_icond2.R
+++ functions/f_process_icond2.R
@@ -2,9 +2,10 @@ f_process_icond2 <- function(icond2_data,parameter){
   ##function to postprocess icon d2 forecast data for visualization
   # - icond2_data:  SpatRaster; icon d2 data, result of f_read_icond2.R
   # - parameter:    character;  icon d2 abbreviations for forecasted parameters
-  #                             options: "rain_gsp" for rain
-  #                                      "snow_gsp" for snow
-  #                                      "t_2m"     for temperature 2m above ground
+  #                        options: "tot_prec" for precipitation
+  #                                 "t_2m"     for temperature 2m above ground
+  #                                 "clct"     for cloud cover
+  #                                 "pmsl"     for sea level pressure
   
   #take out forecasts for full hours and convert into SpatRaster
   icond2_subset <- lapply(icond2_data,terra::subset,subset = c(1))
@@ -14,6 +15,9 @@ f_process_icond2 <- function(icond2_data,parameter){
   # -> results in moment forecast not sum over all forecasts
   if (parameter %in% c("tot_prec")){
     diff_forecasts <- diff(all_forecasts)
+  } else if (parameter %in% c("pmsl")){
+    #change Pa into hPa
+    diff_forecasts <- all_forecasts /100
   } else{
     diff_forecasts <- all_forecasts
   }
