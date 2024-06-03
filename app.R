f422d3718e3e955a189e85dbcd0742f26bce2a14
m-kehl <rudolfsinger36@gmail.com>
2024-06-03 12:29:05 +0000

    make shiny-module for impressum

--- app.R
+++ app.R
@@ -68,39 +68,8 @@ ui <- fluidPage(
           tabPanel("Impressum",
                    value = "impressum",
             div(class = "impressum_style",
-            uiOutput("laubfrosch",style="float:right"),
-            h3("Kontakt"),
-            h4("M. Kehl"),
-            h4("mkehl.laubfrosch@gmail.com"),
-            h4("Quellcode auf", tags$a(href="https://github.com/m-kehl/weather_icond2",
-                                       "GitHub")),
-            h3("Haftungsausschluss"),
-            h4("Der Autor übernimmt keine Gewähr für die Richtigkeit, Genauigkeit,
-            Aktualität, Zuverlässigkeit und Vollständigkeit der Informationen.
-            Haftungsansprüche gegen den Autor wegen Schäden materieller oder
-               immaterieller Art, die aus dem Zugriff oder der Nutzung bzw.
-               Nichtnutzung der veröffentlichten Informationen, durch Missbrauch
-               der Verbindung oder durch technische Störungen entstanden sind,
-               werden ausgeschlossen."),
-            p(),
-            h4("Alle Angebote sind freibleibend. Der Autor behält es sich
-               ausdrücklich vor, Teile der Seiten oder das gesamte Angebot ohne 
-               gesonderte Ankündigung zu verändern, zu ergänzen, zu löschen oder
-               die Veröffentlichung zeitweise oder endgültig einzustellen."),
-            h3("Haftungsausschluss für Inhalte und Links"),
-            h4("Verweise und Links auf Webseiten Dritter liegen ausserhalb
-               unseres Verantwortungsbereichs. Es wird jegliche Verantwortung
-               für solche Webseiten abgelehnt. Der Zugriff und die Nutzung
-               solcher Webseiten erfolgen auf eigene Gefahr des jeweiligen Nutzers."),
-            h3("Urheberrechtserklärung"),
-            h4("Die Urheber- und alle anderen Rechte an Inhalten, Bildern, Fotos
-               oder anderen Dateien auf dieser Website, gehören ausschliesslich
-               den genannten Rechteinhabern. Für die Reproduktion jeglicher 
-               Elemente ist die schriftliche Zustimmung des Urheberrechtsträgers
-               im Voraus einzuholen."),
-            h3("Quelle Impressum"),
-            h4(tags$a(href="https://brainbox.swiss/impressum-generator-schweiz/",
-                      "BrainBox Solutions")))
+                impressumUI("impressum")
+            )
           ),
 ## -- B.2 --  TabPanel 2: phenology  -------------------------------------------
 
@@ -322,20 +291,7 @@ server <- function(input, output, session) {
   #device_width <- reactive(shinybrowser::get_width() -40)
   
 ## -- C.1 --  TabPanel 1: Impressum --------------------------------------------
-  
-  # picture of laubfrosch
-  output$laubfrosch <- renderUI({
-        tags$img(src="laubfrosch_blau.png", height=250)
-  })
-  
-  # change picture of laubfrosch on click
-  onclick(
-    "laubfrosch", {
-      #on click -> changing color
-      output$laubfrosch <- renderUI({
-            tags$img(src="laubfrosch_rot.png", height=250)
-      })
-  })
+  impressumServer("impressum")
 ## -- C.2 --  TabPanel 2: phenology  -------------------------------------------
   ## read and process phenology data
   # read meta data
---
+++ functions/impressumUI.R
@@ -0,0 +1,35 @@
+impressumUI <- function(id) {
+  uiOutput(NS(id,"laubfrosch"),style="float:right"),
+  h3("Kontakt")
+  # h4("M. Kehl")
+  # h4("mkehl.laubfrosch@gmail.com")
+  # h4("Quellcode auf", tags$a(href="https://github.com/m-kehl/weather_icond2",
+  #                            "GitHub"))
+  # h3("Haftungsausschluss")
+  # h4("Der Autor übernimmt keine Gewähr für die Richtigkeit, Genauigkeit,
+  #           Aktualität, Zuverlässigkeit und Vollständigkeit der Informationen.
+  #           Haftungsansprüche gegen den Autor wegen Schäden materieller oder
+  #              immaterieller Art, die aus dem Zugriff oder der Nutzung bzw.
+  #              Nichtnutzung der veröffentlichten Informationen, durch Missbrauch
+  #              der Verbindung oder durch technische Störungen entstanden sind,
+  #              werden ausgeschlossen.")
+  # p()
+  # h4("Alle Angebote sind freibleibend. Der Autor behält es sich
+  #              ausdrücklich vor, Teile der Seiten oder das gesamte Angebot ohne 
+  #              gesonderte Ankündigung zu verändern, zu ergänzen, zu löschen oder
+  #              die Veröffentlichung zeitweise oder endgültig einzustellen.")
+  # h3("Haftungsausschluss für Inhalte und Links")
+  # h4("Verweise und Links auf Webseiten Dritter liegen ausserhalb
+  #              unseres Verantwortungsbereichs. Es wird jegliche Verantwortung
+  #              für solche Webseiten abgelehnt. Der Zugriff und die Nutzung
+  #              solcher Webseiten erfolgen auf eigene Gefahr des jeweiligen Nutzers.")
+  # h3("Urheberrechtserklärung")
+  # h4("Die Urheber- und alle anderen Rechte an Inhalten, Bildern, Fotos
+  #              oder anderen Dateien auf dieser Website, gehören ausschliesslich
+  #              den genannten Rechteinhabern. Für die Reproduktion jeglicher 
+  #              Elemente ist die schriftliche Zustimmung des Urheberrechtsträgers
+  #              im Voraus einzuholen.")
+  # h3("Quelle Impressum")
+  # h4(tags$a(href="https://brainbox.swiss/impressum-generator-schweiz/",
+  #           "BrainBox Solutions"))
+}
