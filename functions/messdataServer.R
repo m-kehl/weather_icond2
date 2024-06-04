6b25c574122f67e4f9d09c5b31ae34d3665268cf
m-kehl <rudolfsinger36@gmail.com>
2024-06-04 12:15:34 +0200

    make module for measurement data

---
+++ functions/messdataServer.R
@@ -0,0 +1,64 @@
+messdataServer <- function(id,main_active) {
+  moduleServer(id, function(input, output, session) {
+    
+    output$test <- renderText(paste0("hello","_33"))
+    
+    # update UI
+    shinyjs::hideElement("box_sincetill")
+
+    ## read and process measurement data
+    # read meta data
+    mess_meta <- f_read_mess_meta()
+    # update UI based on meta data
+    updateSelectizeInput(session,"mess_name",
+                         choices = base::unique(mess_meta$Stationsname),
+                         #selected = base::unique(mess_meta$Stationsname)[1],
+                         options = list(maxItems = 5))
+    # read measurement data
+    mess_data <- reactive(f_read_mess(input$mess_name,mess_meta,sub(paste0(id,"-"),"",input$mess_tabsets)))
+
+    ## show information box
+    observeEvent(input$info_mess, {
+      f_infotext(main_active())
+    })
+
+    ## update UI
+    observeEvent(sub(paste0(id,"-"),"",input$mess_tabsets),{
+      f_updateselectize_parameters(session,"parameter_plot1",sub(paste0(id,"-"),"",input$mess_tabsets),input$parameter_plot1)
+      f_updateselectize_parameters(session,"parameter_plot2",sub(paste0(id,"-"),"",input$mess_tabsets),input$parameter_plot2)
+    })
+
+    ## plot measurement data
+    observe({
+      # placeholder-plot if no station is chosen in UI
+      if (length(input$mess_name) == 0){
+        output$mess_plot <- renderPlot(f_plot_placeholder())
+        output$mess_plot_daily <- renderPlot(f_plot_placeholder())
+        output$mess_plot_monthly <- renderPlot(f_plot_placeholder())
+      } else if (sub(paste0(id,"-"),"",input$mess_tabsets) == "now"){
+        # plot measurement data for recent measurements
+        output$mess_plot <- renderPlot(f_plot_mess(mess_data(),sub(paste0(id,"-"),"",input$mess_tabsets),input$parameter_plot1,"Plot 1: "))
+        output$mess_plot_prec <- renderPlot(f_plot_mess(mess_data(),sub(paste0(id,"-"),"",input$mess_tabsets),input$parameter_plot2,"Plot 2: "))
+        shinyjs::hideElement("box_sincetill")
+      } else if (sub(paste0(id,"-"),"",input$mess_tabsets) == "daily"){
+        # update UI
+        updateSliderInput(session,"sincetill",
+                          min = mess_data()$MESS_DATUM[1],
+                          max = mess_data()$MESS_DATUM[nrow(mess_data())],
+                          timeFormat = "%d.%m.%Y")
+        shinyjs::showElement("box_sincetill")
+        # plot measurement data for daily measurements
+        output$mess_plot_daily <- renderPlot(f_plot_mess(mess_data(),sub(paste0(id,"-"),"",input$mess_tabsets),input$parameter_plot1,"Plot 1: ",input$sincetill))
+        output$mess_plot_daily_prec <- renderPlot(f_plot_mess(mess_data(),sub(paste0(id,"-"),"",input$mess_tabsets),input$parameter_plot2,"Plot 2: ",input$sincetill))
+      } else if (sub(paste0(id,"-"),"",input$mess_tabsets) == "monthly"){
+        # plot measurement data for monthly measurements
+        output$mess_plot_monthly <- renderPlot(f_plot_mess(mess_data(),sub(paste0(id,"-"),"",input$mess_tabsets),input$parameter_plot1,"Plot 1: "))
+        output$mess_plot_monthly_prec <- renderPlot(f_plot_mess(mess_data(),sub(paste0(id,"-"),"",input$mess_tabsets),input$parameter_plot2,"Plot 2: "))
+        # update UI
+        shinyjs::hideElement("box_sincetill")
+      }
+      })
+    
+    
+  })
+}
---
+++ functions/messdataUI.R
@@ -0,0 +1,79 @@
+messdataUI <- function(id) {
+  tagList(
+    # tags$head(
+    #   ## define superordinate settings 
+    #   # HTML style for tabsets and notifications
+    #   
+    #   tags$style(HTML(
+    #     "    
+    #       #mess_tabsets > li {
+    #         padding-top: 5px;
+    #   }
+    #     "))),
+
+    column(3, 
+           br(),
+           column(1,       
+                  actionButton(NS(id,"info_mess"), label = NULL, icon = icon("info"),
+                               style="color: black;
+                                        background-color: HoneyDew;
+                                        border-color: #3dc296",
+                               widht = "10%")),
+           column(10,
+                  div(class = "subtitle",h4("Messdaten",width = "90%"))),
+           br(),
+           br(),
+           hr(),
+           selectizeInput(
+             inputId = NS(id,"mess_name"),
+             label = "Stationsname/n (max. 5)",
+             choices = c(""),
+             multiple = TRUE,
+             options = list(maxItems = 5)),
+           selectizeInput(
+             inputId = NS(id,"parameter_plot1"),
+             label = "Parameter für Plot 1 (max. 2)",
+             choices = c(meteo_parameters$parameter),
+             multiple = TRUE,
+             options = list(maxItems = 2),
+             selected = c("Temperatur")
+           ),
+           selectizeInput(
+             inputId = NS(id,"parameter_plot2"),
+             label = "Parameter für Plot 2 (max. 2)",
+             choices = c(meteo_parameters$parameter),
+             multiple = TRUE,
+             options = list(maxItems = 2),
+             selected = c("Niederschlag","relative Feuchte")
+           ),
+           box(id = NS(id,"box_sincetill"),
+               width = '800px',
+               sliderInput(NS(id,"sincetill"),
+                           "Zeitspanne für Plot",
+                           min = Sys.Date()-61,
+                           max = Sys.Date(),
+                           value = c(Sys.Date()-60,Sys.Date()-1),
+                           step = 1)), #1day
+           f_copyright_DWD(),
+           textOutput(NS(id,"test"))),
+    column(9,
+           # define second-level tabsets (now, daily, monthly)
+           # useShinyjs(),
+           # runjs("#mess_tabsets{padding-top: 5px"),
+           tabsetPanel(id = NS(id,"mess_tabsets"),
+                       tabPanel("aktuelle Messungen",
+                                value = NS(id,"now"),
+                                div(class = "map_plot",plotOutput(NS(id,"mess_plot"))),
+                                div(class = "map_plot",plotOutput(NS(id,"mess_plot_prec")))),
+                       tabPanel("Tageswerte",
+                                value = NS(id,"daily"),
+                                div(class = "map_plot",plotOutput(NS(id,"mess_plot_daily"))),
+                                div(class = "map_plot",plotOutput(NS(id,"mess_plot_daily_prec")))),
+                       tabPanel("Monatswerte",
+                                value = NS(id,"monthly"),
+                                div(class = "map_plot",plotOutput(NS(id,"mess_plot_monthly"))),
+                                div(class = "map_plot",plotOutput(NS(id,"mess_plot_monthly_prec"))))
+           )
+    )
+  )
+}
