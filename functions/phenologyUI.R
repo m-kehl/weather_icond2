5377cdeb69cba44136f817ef54b36643d0a7783a
m-kehl <rudolfsinger36@gmail.com>
2024-06-10 13:41:18 +0200

    comments added

--- functions/phenologyUI.R
+++ functions/phenologyUI.R
@@ -1,43 +1,58 @@
+## User Interface for the phenology part
+#  -> left hand side: Buttons/Input fields to select what should be plotted
+#  -> middle: space for plots
+#  -> right hand side: checkboxes to select which help lines should be shown in plot
 phenologyUI <- function(id) {
   tagList(
+    # left hand side
     column(3, 
             br(),
+            #info-botton
             column(1,
                    actionButton(NS(id,"info_pheno"), label = NULL, icon = icon("info"),
                                 style="color: black; 
                                               background-color: HoneyDew; 
                                               border-color: #3dc296")),
+            #subtitle
             column(10,div(class = "subtitle",h4("Phänologie"))),
             br(),
             br(),
             hr(),
+            #which kind of plant should be plotted
             selectInput(
               inputId = NS(id,"pflanzen"),
               label = "Pflanzenart",
               choices = pflanzen_arten,
               multiple = FALSE),
+            #which phase of plant should be plotted
             radioButtons(
               inputId = NS(id,"phase"),
               label = "phänologische Phase",
               choiceNames = phenology_phases$phase,
               choiceValues = phenology_phases$phase_id,
               selected = character(0)),
+            #station names for which federal state should be selectable
             selectInput(
               inputId = NS(id,"bl_plant"),
               label = "Bundesland",
               choices = c(""),
               multiple = FALSE),
+            #data of which station should be plotted
             selectizeInput(
               inputId = NS(id,"station_name"),
               label = "Stationsname/n",
               choices = c("Adelsheim"),
               selected = c("Adelsheim"),
               multiple = TRUE),
+            #copyright DWD
             f_copyright_DWD()),
+    #middle and right hand side
     column(9,
+           #middle -> phenology plot
            column(10,
                   div(class = "map_plot",plotOutput(NS(id,"plant_out"))),
                   textOutput(NS(id,"no_plant"))),
+           #right hand side -> checkboxes for helplines
            column(2,
                   br(),
                   br(),
@@ -49,9 +64,8 @@ phenologyUI <- function(id) {
                       checkboxInput(NS(id,"mtline"),"Monatslinien"),
                       checkboxInput(NS(id,"grid"),"Gitternetz")
                       )),
-           column(6,plotOutput(NS(id,"plant_map")))),
-           
-           #column(6,tableOutput("plant_table"))
-    
+           #middle -> map showing where phenology station/s is/are
+           column(6,plotOutput(NS(id,"plant_map")))
+    ),
   )
 }
