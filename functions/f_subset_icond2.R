03f3dbbedc1210a0b44f35191769421ffc0ac2df
m-kehl <rudolfsinger36@gmail.com>
2024-06-17 06:43:30 +0000

    delete unnessecary function and rewrite subset-function to work on its own

--- functions/f_subset_icond2.R
+++ functions/f_subset_icond2.R
@@ -1,6 +1,6 @@
-f_subset_icond2 <- function(input_time,icond2_processed,parameter){
-  converter <- f_time_converter()
-  ii <- converter[[parameter]][converter$time == input_time]
-  sub <- subset(icond2_processed,c(ii))
+f_subset_icond2 <- function(input_time,icond2_processed){
+  ## function to retrieve one layer out of SpatRaster for
+  #  the time specified with input_time
+  sub <- icond2_processed[[time(icond2_processed) == input_time]]
   return(sub)
 }
