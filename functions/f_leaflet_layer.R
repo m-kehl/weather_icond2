afc5edecd2dac1308f84849ee4bea87b70126d5e
m-kehl <rudolfsinger36@gmail.com>
2024-06-17 07:35:17 +0000

    additional waiter added and comments added

--- functions/f_leaflet_layer.R
+++ functions/f_leaflet_layer.R
@@ -1,5 +1,5 @@
-f_leaflet_layer <- function(parameter,icond2_processed,icond2_layer,session){
-  ## function to exchange plotted layer on leaflet map
+f_leaflet_layer <- function(parameter,icond2_processed,icond2_layer,session,id){
+  ## function to display SpatRaster layer on leaflet map
   # - parameter:      character; icon d2 abbreviation for forecasted parameter
   #                        options: "tot_prec" for precipitation
   #                                 "t_2m"     for temperature 2m above ground
@@ -7,8 +7,8 @@ f_leaflet_layer <- function(parameter,icond2_processed,icond2_layer,session){
   #                                 "pmsl"     for sea level pressure
   # - icond2_processed: SpatRaster; processed icon d2 data (result of
   #                                 f_process_icond2())
-  # - icond2_layer: SpatRaster; layer of icond2_processed which is to be plotted
-  #                             on the map
+  # - icond2_layer: SpatRaster; specific layer of icond2_processed which is to 
+  #                             be plotted on the map
   # - session: Shiny Session
   
   ## layer preparations
@@ -16,7 +16,8 @@ f_leaflet_layer <- function(parameter,icond2_processed,icond2_layer,session){
   source(paste0(getwd(),"/input.R"),local = TRUE)
   
   # set min and max values for colorbar
-  # process icon d2 data -> NA if precipitation smaller 0.1
+  # for precipitation -> NA if precipitation smaller 0.1
+  # for cloud-cover -> NA if cloud cover smaller 10%
   if (length(parameter) != 0){
     if (parameter == "tot_prec"){
       colorpal <- colorNumeric(colours_precipitation,
@@ -35,24 +36,33 @@ f_leaflet_layer <- function(parameter,icond2_processed,icond2_layer,session){
                                domain = c(min(terra::values(icond2_processed),na.rm = T),
                                           max(terra::values(icond2_processed),na.rm = T)),
                                na.color = "transparent")
-      
       iconlayer <- ifel(icond2_layer < 10, NA, icond2_layer)  
     } else if (parameter == "pmsl"){
       colorpal <- colorNumeric(colours_pressure,
                                domain = c(min(terra::values(icond2_processed),na.rm = T),
                                           max(terra::values(icond2_processed),na.rm = T)),
                                na.color = "transparent")
-      
       iconlayer <- icond2_layer  
     }
     
+    #Show waiter while map is being prepared
+    waiter_show(
+      html = tagList(
+        spin_fading_circles(),
+        "Creating Map.."
+      ),
+      id = c(NS(id,"map_out"))
+    )
+    
     ## add layer to map
+    #  load pre-existing map
     mapModifier <- leafletProxy(
       "map_out", session)
     
+    # clear old layer, add new one
     mapModifier %>%
       clearImages() %>%
-      removeControl(layerId = "intro") %>%
+      removeControl(layerId = "map_intro") %>%
       addRasterImage(iconlayer, col = colorpal,opacity = 0.8)
     waiter_hide()
   }
