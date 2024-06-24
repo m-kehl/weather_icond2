222a7ae625fb880cccdf6656ad294a08ac70d212
m-kehl <rudolfsinger36@gmail.com>
2024-06-24 11:33:25 +0000

    comment added

--- functions/f_subset_icond2.R
+++ functions/f_subset_icond2.R
@@ -1,6 +1,9 @@
 f_subset_icond2 <- function(input_time,icond2_processed){
   ## function to retrieve one layer out of SpatRaster for
   #  the time specified with input_time
+  # - input_time: POSIXct; date/time for layer in question
+  # - icond2_processed: SpatRaster; data from which layer is retrieved, ie 
+  #                                 result of f_process_icond2()
   sub <- icond2_processed[[time(icond2_processed) == input_time]]
   return(sub)
 }
